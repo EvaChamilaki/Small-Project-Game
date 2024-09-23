af4e660d00dc56bdd63f88c7584e3fb3d92eb32d
andierm <adyrm98@gmail.com>
2024-09-23 13:50:30 +0200

    switch to computer camera

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-    [SerializeField]
+    [Header("UI Elements")]
+    public GameObject canvasClick;
+
+    [Header("Character Control")]
     public CharacterController charcontroller;
     public Transform camera;
 
@@ -12,6 +17,17 @@ public class ThirdPersonCamera : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     public float turnSmoothVel;
 
+    private bool computerHit = false;
+
+    private Camera main_camera;
+    private Camera computer_camera;
+
+    void Start()
+    {
+        canvasClick.SetActive(false);
+        main_camera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        computer_camera = GameObject.FindWithTag("ComputerCamera").GetComponent<Camera>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -30,7 +46,36 @@ public class ThirdPersonCamera : MonoBehaviour
             charcontroller.Move(moveDirection.normalized * speed * Time.deltaTime);
             
         }
-       
+
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, 4.0f))
+        {
+            if (hit.transform.CompareTag("ComputerScreen"))
+            {
+                computerHit = true;
+                canvasClick.SetActive(true);
+
+            }
+        }
+
+        else
+        {
+            computerHit = false;
+            canvasClick.SetActive(false);
+        }
+
+        if(computerHit && Input.GetMouseButtonDown(0))
+        {
+            main_camera.enabled  = false;
+            computer_camera.enabled = true;
+            canvasClick.SetActive(false );
+        }
+
+
+      
+
 
 
     }
