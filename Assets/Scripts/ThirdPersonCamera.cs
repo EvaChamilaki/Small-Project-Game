ed53a3786bde84ec6f4984ab65b69392c90f4ea6
andierm <adyrm98@gmail.com>
2024-09-23 14:17:09 +0200

    switch back to main camera (temporary)

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -18,7 +18,9 @@ public class ThirdPersonCamera : MonoBehaviour
     public float turnSmoothVel;
 
     private bool computerHit = false;
+    private bool charControlActive = true;
 
+    private Camera active_camera;
     private Camera main_camera;
     private Camera computer_camera;
 
@@ -27,30 +29,36 @@ public class ThirdPersonCamera : MonoBehaviour
         canvasClick.SetActive(false);
         main_camera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
         computer_camera = GameObject.FindWithTag("ComputerCamera").GetComponent<Camera>();
+        computer_camera.enabled = false;
+
+        active_camera = main_camera;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float hor = Input.GetAxis("Horizontal"); //negative for A and left arrow, positive for D and right arrow
-        float ver = Input.GetAxis("Vertical"); //negative for S and down arrow, positive for W and up arrow
 
-        Vector3 direction = new Vector3(hor, 0f, ver).normalized;
-
-        if(direction.magnitude >= 0.1f)
+        if(charControlActive)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            charcontroller.Move(moveDirection.normalized * speed * Time.deltaTime);
-            
+            float hor = Input.GetAxis("Horizontal"); //negative for A and left arrow, positive for D and right arrow
+            float ver = Input.GetAxis("Vertical"); //negative for S and down arrow, positive for W and up arrow
+
+            Vector3 direction = new Vector3(hor, 0f, ver).normalized;
+
+            if (direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+                Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                charcontroller.Move(moveDirection.normalized * speed * Time.deltaTime);
+
+            }
         }
-
-
+        
         RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray.origin, ray.direction, out hit, 4.0f))
+        Ray ray = active_camera.ScreenPointToRay(Input.mousePosition);
+        if (active_camera == main_camera && Physics.Raycast(ray.origin, ray.direction, out hit, 5.0f))
         {
             if (hit.transform.CompareTag("ComputerScreen"))
             {
@@ -68,15 +76,35 @@ public class ThirdPersonCamera : MonoBehaviour
 
         if(computerHit && Input.GetMouseButtonDown(0))
         {
-            main_camera.enabled  = false;
-            computer_camera.enabled = true;
-            canvasClick.SetActive(false );
+            SwitchToComputerCamera();         
         }
 
+        if(!charControlActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            SwitchToMainCamera();
+        }
 
-      
+    }
+    void SwitchToComputerCamera() 
+    {
+        main_camera.enabled = false;
+        computer_camera.enabled = true;
+        canvasClick.SetActive(false);
 
+        charcontroller.enabled = false;
+        charControlActive = false;
 
+        active_camera = computer_camera;
+    }
+    void SwitchToMainCamera()
+    {
+        computer_camera.enabled = false;
+        main_camera.enabled = true;
+
+        charcontroller.enabled = true;
+        charControlActive = true;
+
+        active_camera = main_camera;
 
     }
 }
