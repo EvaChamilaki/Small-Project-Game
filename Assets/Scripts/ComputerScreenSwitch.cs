793d319c338cb7cb499ecbfc1e2266f0859be6a0
andierm <adyrm98@gmail.com>
2024-10-07 11:38:49 +0200

    added a timer to exit the loading screen

--- Assets/Scripts/ComputerScreenSwitch.cs
+++ Assets/Scripts/ComputerScreenSwitch.cs
@@ -7,10 +7,24 @@ public class ComputerScreenSwitch : MonoBehaviour
     public GameObject currentScreen;
     public GameObject newScreen;
 
+    public void LoadingScreenStarts() //animation even to check when the loading screen has been activated
+    {
+        Debug.Log("Loading Screen Starts");
+    }
 
-   public void SwitchScreens()
+   public void SwitchScreens() //for the button to change between screens
     {
         if(currentScreen != null)
+        {
+            currentScreen.SetActive(false);
+            newScreen.SetActive(true);
+        }
+      
+    }
+
+    public void LoadingEnds() //for the animation event to load the new screen when the animation ends
+    {
+        if(currentScreen.name == "LoadingScreen")
         {
             currentScreen.SetActive(false);
             newScreen.SetActive(true);
