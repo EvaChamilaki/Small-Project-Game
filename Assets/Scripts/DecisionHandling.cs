9d834a6d5ed6b72355803a517694bc8c04e347f5
andierm <adyrm98@gmail.com>
2024-10-07 15:01:33 +0200

    made a xazomara for testing

---
+++ Assets/Scripts/DecisionHandling.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecisionHandling : MonoBehaviour
+{
+    public Animator animController;
+    
+  
+
+
+    public void DecisionA()
+    {
+        ChangeEmotionalState("Happy");
+    }
+
+    public void DecisionB()
+    {
+        ChangeEmotionalState("Sad");
+    }
+
+        public void ChangeEmotionalState(string emotion)
+    {
+        GameObject obj = GameObject.FindWithTag("emotion_screen");
+        animController = obj.GetComponent<Animator>();
+        animController.SetTrigger(emotion);
+    }
+ 
+}
