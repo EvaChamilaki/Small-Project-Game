a818e729e7faab0a9e7bbb5ef0f99583a56f6ef0
andierm <adyrm98@gmail.com>
2024-10-08 17:06:59 +0200

    made the camera change when the choice locks

--- Assets/Scripts/DecisionHandling.cs
+++ Assets/Scripts/DecisionHandling.cs
@@ -5,15 +5,41 @@ using UnityEngine;
 public class DecisionHandling : MonoBehaviour
 {
     public Animator animController;
+    private Camera computer_camera;
+    private Camera emotions_camera;
+
+    public GameObject decisionButtons;
     
+    void Start()
+    {
+        computer_camera = GameObject.FindWithTag("ComputerCamera").GetComponent<Camera>();
+        emotions_camera = GameObject.FindWithTag("EmotionsCamera").GetComponent<Camera>();
+    }
     public void DecisionA()
     {
         ChangeEmotionalState("Happy");
+        computer_camera.enabled = false;
+        emotions_camera.enabled = true;
+
+        if(emotions_camera.enabled)
+        {
+            decisionButtons.SetActive(false);
+        }
+
+
     }
 
     public void DecisionB()
     {
         ChangeEmotionalState("Sad");
+        computer_camera.enabled = false;
+        emotions_camera.enabled = true;
+
+        if(emotions_camera.enabled)
+        {
+            decisionButtons.SetActive(false);
+        }
+
     }
 
         public void ChangeEmotionalState(string emotion)
