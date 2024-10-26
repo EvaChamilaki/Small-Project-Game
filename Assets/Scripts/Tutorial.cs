28efefb5e6701db284340629d3015db73558a8e0
Eva <e.chamilaki@students.uu.nl>
2024-10-26 18:55:38 +0200

    oopsies forgot

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -10,7 +10,7 @@ public class Tutorial : MonoBehaviour
     public TextMeshProUGUI tutorialText; //the text that will be shown in the panel
 
     public string tutorialkey; //the key that will track if the tutorial has been shown
-    
+
     public bool isTutorialshown;
     public bool isTutorialActive;
 
@@ -18,25 +18,25 @@ public class Tutorial : MonoBehaviour
     {
         tutorial.SetActive(false);
         isTutorialActive = false;
-        
+
     }
 
     public void ShowTutorial(string message, string key)
     {
-       tutorialkey = key;
-       if(!notfirstTimeShown(tutorialkey))
-       {
-       tutorialText.text = message;
-       tutorial.SetActive(true);
-       isTutorialActive = true;
-       }
+        tutorialkey = key;
+        if (!notfirstTimeShown(tutorialkey))
+        {
+            tutorialText.text = message;
+            tutorial.SetActive(true);
+            isTutorialActive = true;
+        }
 
-    //    foreach(GameObject element in otherUIElements)
-    //    {
-    //        element.SetActive(false);
-    //    }
+        //    foreach(GameObject element in otherUIElements)
+        //    {
+        //        element.SetActive(false);
+        //    }
 
-    Time.timeScale = 0; //pauses the game so no input can be done until the next button is pressed
+        Time.timeScale = 0; //pauses the game so no input can be done until the next button is pressed
 
 
     }
@@ -53,17 +53,17 @@ public class Tutorial : MonoBehaviour
         //     element.SetActive(true);
         // }
 
-    Time.timeScale = 1; //resumes the game
+        Time.timeScale = 1; //resumes the game
     }
 
-   public bool notfirstTimeShown(string key)
-   {
-    return PlayerPrefs.GetInt(key, 0) == 1; //if the tutorial has not been shown before, the key will be 0, if it has been shown the key will be 1
-   }
+    public bool notfirstTimeShown(string key)
+    {
+        return PlayerPrefs.GetInt(key, 0) == 1; //if the tutorial has not been shown before, the key will be 0, if it has been shown the key will be 1
+    }
 
-   private void saveShown(string key)
-   {
-    PlayerPrefs.SetInt(key, 1);
-    PlayerPrefs.Save();
-   }
+    private void saveShown(string key)
+    {
+        PlayerPrefs.SetInt(key, 1);
+        PlayerPrefs.Save();
+    }
 }
