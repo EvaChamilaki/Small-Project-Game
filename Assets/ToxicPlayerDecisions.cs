6207997f0f9de5782aafac855bdc22aa88c3d664
andierm <adyrm98@gmail.com>
2024-11-23 12:21:59 +0100

    second interaction part 1

--- Assets/ToxicPlayerDecisions.cs
+++ Assets/ToxicPlayerDecisions.cs
@@ -10,13 +10,30 @@ public class ToxicPlayerDecisions : MonoBehaviour
     public GameObject firstInteractionPanel;
     public GameObject secondInteractionPanel;
 
+    public GameObject currentScreen;
+
     private ChatBehaviorManager _chatManager;
     public GameObject ChatManagerObject;
+
+    private bool hasStartedTyping = false;
 
     // Start is called before the first frame update
     void Start()
     {
         _chatManager = ChatManagerObject.GetComponent<ChatBehaviorManager>();
+        firstInteractionPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(currentScreen.activeSelf && !hasStartedTyping)
+        {
+            firstInteractionPanel.SetActive(true);
+            hasStartedTyping = true;
+
+            firstInteractionPanel.GetComponent<TextWriting>().enabled = true;
+            firstInteractionPanel.GetComponent<TextWriting>().StartTextTyping(0);
+        }
     }
 
     public void DecisionA_Remind()
@@ -31,6 +48,16 @@ public class ToxicPlayerDecisions : MonoBehaviour
         yield return new WaitUntil(() => _chatManager.messageList.Last().textObj.GetComponent<TextWriting>().textCompleted);
         yield return new WaitForSeconds(1.0f);
         secondInteractionPanel.SetActive(true);
+        secondInteractionPanel.GetComponent<TextWriting>().enabled = true;
+        secondInteractionPanel.GetComponent<TextWriting>().StartTextTyping(0);
     }
+    
+    // private IEnumerator StartTyping()
+    // {
+       
+    //     decisionB1.SetActive(true);
+    //     decisionB1.GetComponent<TextWriting>().enabled = true;
+    //     decisionB1.GetComponent<TextWriting>().StartTextTyping(0); 
+    // }
 
     }
