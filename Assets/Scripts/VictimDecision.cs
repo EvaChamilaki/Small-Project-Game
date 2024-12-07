9389e4da1734336ae476480897d98895a088ee1f
andierm <adyrm98@gmail.com>
2024-12-07 01:31:54 +0100

    more more fixes

--- Assets/Scripts/VictimDecision.cs
+++ Assets/Scripts/VictimDecision.cs
@@ -100,7 +100,7 @@ public class VictimDecision : MonoBehaviour
     }
     public IEnumerator DecisionA_Angry() //first choice is angry
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(1.0f);
         yield return new WaitUntil(() => !character.GetComponent<ThirdPersonCamera>().emotions_camera.enabled);
         question.SetActive(true); //do you act on the angry emotion
         question.GetComponent<TextWriting>().enabled = true;
@@ -155,7 +155,7 @@ public class VictimDecision : MonoBehaviour
 
     public IEnumerator DecisionB_Sad() //first choice is sad
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(1.0f);
         yield return new WaitUntil(() => !character.GetComponent<ThirdPersonCamera>().emotions_camera.enabled);
         panel.SetActive(true); //you did not act panel
     }
